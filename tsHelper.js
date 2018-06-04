9c3223d8db7ae09b7dd7bf2e6f8ee43063ba0b0d
guoshuai <guoshuai@qianmi.com>
2018-06-04 11:43:58 +0800

    add tsHelper

---
+++ tsHelper.js
@@ -0,0 +1,13 @@
+module.exports = {
+  watchDirs: {
+    graphqlClass: {
+      path: 'app/graphql',
+      interface: 'IGraphqlClass',
+      pattern: '**/(model|connector)*.(ts|js)',
+      generator: 'class',
+      caseStyle: 'lower',
+      trigger: ['add', 'unlink'],
+      enabled: true
+    }
+  }
+};
